cbbb98e9a39ae3af18cb9e7b73757f93e504f16c
Yuvraj Panwar <yuvrajpanwar2110@gmail.com>
2025-11-20 10:45:48 +0530

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ehitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
